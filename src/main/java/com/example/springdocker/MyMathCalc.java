9f519b8d6cd4d8c82e7845b21a84e79975b4102d
PatrikJMelander <patrikjmelander@gmail.com>
2021-05-12 13:46:50 +0200

    added class MyMathCalc

---
+++ src/main/java/com/example/springdocker/MyMathCalc.java
@@ -0,0 +1,20 @@
+package com.example.springdocker;
+
+/**
+ * Created by Patrik Melander
+ * Date: 2021-05-12
+ * Time: 13:44
+ * Project: SoloProject
+ * Copyright: MIT
+ */
+public class MyMathCalc {
+    public int add (int a, int b){
+        return a+b;
+    }
+    public int multiply (int a, int b){
+        return a*b;
+    }
+    public int divided (int a, int b){
+        return a/b;
+    }
+}
